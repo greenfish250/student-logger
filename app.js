8a85be56f5f2cf65df1fe9c2b26aa69471ce8cc2
EthanPittman171 <ethanpittman171@gmail.com>
2022-07-16 11:00:53 -0400

    Created function to insert student info into the DOM

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 //DOM elements
 const studentForm = document.querySelector("#studentForm");
-const studentDiv = document.querySelector(".students");
+const studentContainer = document.querySelector(".students");
 const nameInput = studentForm["name"];
 const ageInput = studentForm["age"];
 const rollInput = studentForm["roll"];
+
+function createStudentElement(name, age, roll) {
+  const studentSection = document.createElement("div");
+  const studentName = document.createElement("h2");
+  const studentAge = document.createElement("p");
+  const studentRoll = document.createElement("p");
+
+  studentName.innerText = `Student Name: ${name}`;
+  studentAge.innerText = `Student Age: ${age}`;
+  studentRoll.innerText = `Student Roll: ${roll}`;
+
+  studentSection.append(studentName, studentAge, studentRoll);
+  studentContainer.appendChild(studentSection);
+}
