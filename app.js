8358c5a78e4b60137fe8c392339b310d2696159e
EthanPittman171 <ethanpittman171@gmail.com>
2022-07-16 10:54:15 -0400

    Declare neccessary DOM variables

---
+++ app.js
@@ -0,0 +1,6 @@
+//DOM elements
+const studentForm = document.querySelector("#studentForm");
+const studentDiv = document.querySelector(".students");
+const nameInput = studentForm["name"];
+const ageInput = studentForm["age"];
+const rollInput = studentForm["roll"];
